dc4d2038a6c67066a61f5f9457bc8ff6e833ec45
cejimenez0123 <christianjimenez0123@gmail.com>
2020-03-02 10:43:34 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 let addToy = false;
 let toyCollection = document.getElementById('toy-collection');
-let addToyForm = document.getElementsByClassName('add-toy-form')
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyForm = document.querySelector(".container");
-  
-  addBtn.addEventListener("click", () => {
-    addToy = !addToy;
-    if (addToy) {
-      toyForm.style.display = "block";
-      
-    } else {
-      toyForm.style.display = "none";
-    }
-  });
-});
+let addToyForm = document.getElementsByClassName('add-toy-form');
+
 function postToy(toy_data) {
-  debugger
   fetch('http://localhost:3000/toys', {
       method: 'POST',
       headers: {
@@ -32,9 +18,25 @@ function postToy(toy_data) {
     })
     .then(res => res.json())
     .then((obj_toy) => {
-    renderToys(obj_toy)   
-    })
+      let newToy = renderToys(obj_toy)
+      toyCollection.append(newToy)
+  })
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toyForm = document.querySelector(".container");
+  const addBtn = document.querySelector("#new-toy-btn");
+  addBtn.addEventListener("click", () => {
+    addToy = !addToy;
+    if (addToy) {
+      toyForm.style.display = "block";
+      
+    } else {
+      toyForm.style.display = "none";
+    }
+  });
+});
+
 function getToys() {
   return fetch('http://localhost:3000/toys')
     .then(res => res.json())
@@ -47,8 +49,28 @@ getToys().then(toys => {
   addToyForm.addEventListener('submit', event => {
     event.preventDefault()
     postToy(event.target)})
-  function renderToys(toy) {
 
+  function likes(e) {
+      e.preventDefault()
+      let more = parseInt(e.target.previousElementSibling.innerText) + 1
+    
+      fetch(`http://localhost:3000/toys/${e.target.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+    
+          },
+          body: JSON.stringify({
+            "likes": more
+          })
+        })
+        .then(res => res.json())
+        .then((like_obj => {
+          e.target.previousElementSibling.innerText = `${more} likes`;
+        }))
+    }
+  function renderToys(toy) {
   toyCollection = document.getElementById('toy-collection');
     let h2 = document.createElement('h2')
     h2.innerText = toy.name
@@ -74,6 +96,7 @@ getToys().then(toys => {
     appendChildren(divCard,children)
     toyCollection.appendChild(divCard)
   }
+
   function appendChildren(parent,children){
     children.forEach(child=>{ parent.appendChild(child)})
   }
